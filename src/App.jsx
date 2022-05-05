a625731cc0a08afbc8cad72555d659b557a245f5
Luis Henrique <luis.henrique@wapstore.com.br>
2022-05-05 15:51:22 -0300

    Alterando json de entrada

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
 
      useEffect(() => {
           const fetchTasks = async ()=> {
-               const {data} = await axios.get('https://jsonplaceholder.cypress.io/todos?_limit=1');
+               const {data} = await axios.get('https://jsonplaceholder.cypress.io/todos?_limit=0');
                setTasks(data);
           };
 
