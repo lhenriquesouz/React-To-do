739af743387307c47994b48808b8f581a264235d
Luis Henrique <luis.henrique@wapstore.com.br>
2022-05-05 13:34:05 -0300

    Modificando numero de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
 
      useEffect(() => {
           const fetchTasks = async ()=> {
-               const {data} = await axios.get('https://jsonplaceholder.cypress.io/todos?_limit=5');
+               const {data} = await axios.get('https://jsonplaceholder.cypress.io/todos?_limit=1');
                setTasks(data);
           };
 
